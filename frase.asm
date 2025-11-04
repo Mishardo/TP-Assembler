df127b49413d410f5d97d07190fbe6272efc01fe
ThiagoRositi <110995578+ThiagoRositi@users.noreply.github.com>
2025-11-03 23:31:42 -0300

    Add files via upload

---
+++ frase.asm
@@ -0,0 +1,97 @@
+.8086
+.model small
+.stack 100h
+.data
+    arch1		db 'frases.txt',0
+    buf1		db 4096 dup(?)
+    fraseSeleccionada db ?
+    cartel db "error abrir archivo",0ah,0dh,24h
+
+.code
+
+    PUBLIC frase 
+
+    frase proc 
+    
+    push bp
+    mov bp, sp
+
+    mov ah, 3Dh
+    mov al, 0
+    mov dx, offset arch1
+    int 21h
+    jc errorAbrirArchivo
+
+    mov bx, ax        ; handle
+    mov ah, 3Fh
+    mov cx, 4095
+    int 21h           ; lee hasta 255 bytes
+
+    mov ah, 3Eh
+    int 21h
+
+    ;Seleccionar aleatorio entre 0 y 50
+    mov ah, 00h
+    int 1Ah
+    mov ax, dx
+    mov bx, 50
+    xor dx, dx
+    div bx
+    mov fraseSeleccionada, dl
+
+    
+
+    call seleccionarFrase
+
+    pop bp
+
+    ret
+
+    errorAbrirArchivo:
+    mov bx, offset cartel
+    ret
+
+    frase endp
+
+    ; luego quiero agregarle un nro aleatorio y que ponga el offset al inicio de luego de un salto de linea y el signo pesos al final de esa linea
+    seleccionarFrase proc
+
+    mov si, offset buf1
+    mov cl, 1                ; contador de líneas actuales
+    mov al, fraseSeleccionada
+
+    buscar_inicio:
+        cmp cl, al
+        je  linea_encontrada        ;cuenta los saltos de linea para encontrar la linea aleatoria que toca.
+        cmp byte ptr [si], 0
+        je  fin_archivo
+        cmp byte ptr [si], 0Ah
+        jne continuar
+        inc cl
+    continuar:
+        inc si
+        jmp buscar_inicio
+
+    linea_encontrada:
+        mov bx, si              ; guardar inicio de la línea seleccionada
+    buscar_fin:
+        cmp byte ptr [si], 0        ;recorre para encontrar el final
+        je fin_linea
+        cmp byte ptr [si], 0Ah
+        je fin_linea
+        inc si
+        jmp buscar_fin
+
+    fin_linea:
+        mov byte ptr [si], '$'  ; poner fin de cadena
+        ret
+
+    fin_archivo:
+        mov byte ptr [si], '$'
+        ret
+
+    seleccionarFrase endp
+
+
+end
+
